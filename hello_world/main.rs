997fb2adb3bfc65efa6f2a19029ccc1bada90a59
goat2130 <yagisawahodaka@team-lab.com>
2025-02-14 09:49:14 +0900

    Add .gitignore to exclude problematic directory

---
+++ hello_world/main.rs
@@ -0,0 +1,12 @@
+// use ferris_says::say; // from the previous sep
+// use std::io::{stdout, BufWriter};
+
+fn main() {
+    // let stdout = stdout();
+    // let message = "Hello, fellow Rustaceans!";
+    // let width = message.chars().count();
+
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(&message, width, &mut writer).unwrap();
+    println!("Hello, world!");
+}
