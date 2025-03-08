e9944a9a90392800779f3af0b88054bb08586088
Goat <goat229@icloud.com>
2025-03-08 09:09:17 +0900

    add

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -50,6 +50,7 @@ fn main() {
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
+
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
@@ -80,7 +81,7 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>(",10", ','), None);
     assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
-    assert_eq!(parse_pair::<i32>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<i32>("0.5x", 'x), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
@@ -103,9 +104,10 @@ fn test_pixel_to_point() {
     assert_eq!(
         pixel_to_point((100, 200), (25, 175),
             Complex{ re: -1.0, im: 1.0},
-            Complex{ re: 1.0, im: -1.0}),
-        Complex{ re: -0.5, im: -0.75}
-    );
+            Complex{ re: 1.0, im: -1.0},
+            Complex{ re: -0.5, im: -0.75}
+        )
+    )
 }
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
