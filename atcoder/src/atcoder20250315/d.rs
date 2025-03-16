dc8fdff6c51b54c40258e883dd2352f4663a002c
Goat <goat229@icloud.com>
2025-03-16 20:39:37 +0900

    update:

--- atcoder/src/atcoder20250315/d.rs
+++ atcoder/src/atcoder20250315/d.rs
@@ -1,28 +1,42 @@
-use proconio::input;
+use std::io::{self, BufRead};
 
-fn main() {
-    input! {
-        n: i64,
-    }
-
-    let limit = ((n.abs() as f64).cbrt().ceil() as i64) + 1;
-
-    for x in -limit..=limit {
-        let mut low = -limit;
-        let mut high = limit;
-        while low <= high {
-            let mid = (low + high) / 2;
-            let diff = x.pow(3) - mid.pow(3);
-            if diff == n {
-                println!("{} {}", x, mid);
-                return;
-            } else if diff < n {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+fn sol(a: i64, b: i64, c: i64) -> i64 {
+    // ax^2 + bx + c = 0の解
+    let mut l = 0;
+    let mut r = 600000001;
+    while r - l > 1 {
+        let mid = (l + r) / 2;
+        if a * mid * mid + b * mid + c <= 0 {
+            l = mid;
+        } else {
+            r = mid;
         }
     }
+    if a * l * l + b * l + c == 0 {
+        return l;
+    }
+    -1
+}
 
+fn main() {
+    let stdin = io::stdin();
+    let mut iterator = stdin.lock().lines();
+    let n: i64 = iterator.next().unwrap().unwrap().trim().parse().unwrap();
+
+    for d in 1.. {
+        if d * d * d > n {
+            break;
+        }
+        // (k+d)^3 - k^3 = d^3 + 3*d^2*k + 3*d*k^2 = n
+        if n % d != 0 {
+            continue;
+        }
+        let m = n / d; // =3*k^2 + 3*dk + d^2
+        let k = sol(3, 3 * d, d * d - m);
+        if k > 0 {
+            println!("{} {}", k + d, k);
+            return;
+        }
+    }
     println!("-1");
 }
