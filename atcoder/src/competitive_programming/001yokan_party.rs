4f0eee42188bb65370d8241b8878ea5286bc0070
Goat <goat229@icloud.com>
2025-04-24 06:45:07 +0900

    update: create yokan_oarty

--- atcoder/src/competitive_programming/001yokan_party.rs
+++ atcoder/src/competitive_programming/001yokan_party.rs
@@ -1,1 +1,18 @@
 use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            if a[i] == a[j] {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
+}
